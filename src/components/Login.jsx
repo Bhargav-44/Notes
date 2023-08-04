d27639711ba513c0f567cc1871d2fc0abaf90118
Bhargav Varma <112095470+Bhargav-44@users.noreply.github.com>
2023-08-04 23:35:53 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
               <span className="button_top">Login</span>
             </button>
             <p class="signin">
-              Don't have an acount ? <span className="text-blue-700 underline cursor-pointer" onClick={()=>setNewUser(true)}>Sign Up</span>{" "}
+              Don't have an acount ? <span className="text-blue-700 underline cursor-pointer" onClick={()=>setNewUser(false)}>Sign Up</span>{" "}
             </p>
           </form>
         </div>
